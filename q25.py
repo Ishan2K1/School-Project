3cfc4faedc7ca6158d4724dd89653092fbf009fd
Ishan2K1 <khandelwalishan@rocketmail.com>
2018-10-05 22:27:09 +0530

    Q25

---
+++ q25.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 21:24:40 2018
+
+@author: Ishan Khandelwal
+"""
+l1=[]
+l2=[]
+while True:
+    a=input("Enter Number Here")
+    if a=="":
+        break
+    else:
+        l1.append(int(a))
+print(l1)
+for i in l1:
+    if i not in l2:
+        l2.append(i)
+        print("*"*l1.count(i))
