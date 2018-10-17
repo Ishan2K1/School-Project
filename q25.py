50baba0a0df4cda12a196b7739b639cb661e74c3
Ishan2K1 <43716776+Ishan2K1@users.noreply.github.com>
2018-10-17 22:18:04 +0530

    Update q25.py

--- q25.py
+++ q25.py
@@ -1,85 +1,19 @@
-import os
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 21:24:40 2018
 
-import time
-
-WIDTH = 79
-j=100
-
-message = "noob!".upper()
-
-printedMessage = [ "","","","","","","" ]
-
-characters = { " " : [ " ",
-                       " ",
-                       " ",
-                       " ",
-                       " ",
-                       " ",
-                       " " ],
-
-               "O" : [ "******",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "******" ],
-               
-               "N" : [ "*     *",
-                       "**    *",
-                       "* *   *",
-                       "*  *  *",
-                       "*   * *",
-                       "*    **",
-                       "*     *" ], 
-
-               "B" : [ "*****",
-                       "*   *",
-                       "*   *",
-                       "***",
-                       "*   *",
-                       "*   *",
-                       "*****" ],
-
-               "L" : [ "******",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "*    *",
-                       "******" ],
-
-               "!" : [ "  *  ",
-                       "  *  ",
-                       "  *  ",
-                       "  *  ",
-                       "  *  ",
-                       "     ",
-                       "  *  " ]
-               
-               }
-
-#build up the printed banner. to do this, the 1st row of the
-#display is created for each character in the message, followed by
-#the second line, etc..
-for row in range(7):
-    for char in message:
-        printedMessage[row] += (str(characters[char][row]) + "  ")
-
-#the offset is how far to the right we want to print the message.
-#initially, we want to print the message just off the display.
-offset = WIDTH
-while j!=0:
-    os.system("cls")
-    #print each line of the message, including the offset.
-    for row in range(7):
-        print(" " * offset + printedMessage[row][max(0,offset*-1):WIDTH - offset])
-    #move the message a little to the left.
-    offset -=1
-    #if the entire message has moved 'through' the display then
-    #start again from the right hand side.
-    if offset <= ((len(message)+2)*6) * -1:
-        offset = WIDTH
-    #take out or change this line to speed up / slow down the display
-    time.sleep(0.05)
-    j-=1
+@author: Ishan Khandelwal
+"""
+l1=[]
+l2=[
+while True:
+    a=input("Enter Number Here")
+    if a=="":
+        break
+    else:
+        l1.append(int(a))
+print(l1)
+for i in l1:
+    if i not in l2:
+        l2.append(i)
+        print("*"*l1.count(i))
