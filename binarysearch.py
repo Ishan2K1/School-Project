c1bc98b0d653fd0fa4c87d2a1531b0e33408394f
Ishan2K1 <khandelwalishan@rocketmail.com>
2018-10-18 21:39:16 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,36 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 17 18:00:02 2018
-
-@author: Ishan Khandelwal
-"""
 l1=[]
+l2=[]
 while True:
     m=input("enter numbers here")
     if m!="":
         l1.append(int(m))
     else:
         break
+for i in range(len(l1)):
+    l2.append(i)
+dic=dict(zip(l1,l2))
 l1.sort()
 x=int(input("Enter Number to Find here"))
 while True:
-    p=int(len(l1)/2)
-    if x<l1[p-1]:
-        l1=l1[0:p+1:1]
-        if p==1:
-            if p!=l1[0] and p!=l1[1]:
+    p=round(len(l1)/2)
+    if p==0:
+        p=1
+    if x<l1[p]:
+        l1=l1[0:p]
+        if p==1 or len(l1)==1:
+            if x!=l1[0]:
                 print("not found")
                 break
     elif x>l1[p]:
-        l1=l1[p:len(l1)+1:1]
-        if p==1:
-            if p!=l1[0] and p!=l1[1]:
+        l1=l1[p:len(l1)]
+        print(l1)
+        if p==1 or len(l1)==1:
+            if x!=l1[0]:
                 print("not found")
                 break
     elif p==1:
         if l1[0]==x:
-            print("found")
+            print("found",dic.get(x), "th position")
             break
         elif l1[1]==x:
-            print("found")
+            print("found",dic.get(x),"th position")
             break
+    elif x==l1[p]:
+        print("found",dic.get(x), "th position")
+        break
