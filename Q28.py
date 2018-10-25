e62fa4a6edd01ee6e53061549b75d34767e2ffd3
Ishan Khandelwal <khandelwalishan@rocketmail.com>
2018-10-25 13:21:54 +0530

    Update Q28.py

--- Q28.py
+++ Q28.py
@@ -1,5 +1,6 @@
-#Insertion Sort
+#insertion Sort
 l1=[]
+
 while True:
     m=input("Enter Number here")
     if m!="":
@@ -8,12 +9,12 @@ while True:
         break
 count=0
 for i in range(1,len(l1)):
-    key=l1[i]
     j=i-1
-    while j>=0 and key<l1[j]:
+    k=l1[i]
+    while j>=0 and k<l1[j]:
         l1[j+1]=l1[j]
-        j-=1
+        j=j-1
         count+=1
-    l1[j+1]=key
+    l1[j+1]=k
         
 print(l1, count)
