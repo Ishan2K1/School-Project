7ae0e60954eb2f6ad5759afcd39788726ca923b1
Ishan Khandelwal <khandelwalishan@rocketmail.com>
2018-10-26 13:30:23 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,27 +1,28 @@
 #matrix
+#matrix
 n=int(input("Enter number of rows here"))
 m=int(input("Enter number of columns here"))
 l2=[]
-columnSum=0
+cSum=0
 for i in range(n):
     l=[]
     j=m
     while j>0:
         f=int(input("Enter Number here"))
-        l.append(f)
+        l.append(f) 
         j-=1
     l2.append(l)
+x=0 
 for i in l2:
-    print(i)
-x=0
-for i in l2:
+    print(l2[x],sum(i),max(i))
     x+=1
-    print(max(i)," is max in row no.", x)
-x=0
-for i in l2:
-    x+=1
-    print(sum(i), " is the sum of numbers in row", x)
-for i in l2:
-    
-    for j in range(len(l2)):
-        columnSum=       #incomplete
+for j in range(n):
+    for i in range(m):
+        cSum+=l2[i][j]
+    print(cSum, end="\t")
+    cSum=0
+dSum=0
+f=0
+for i in range(n):
+    for j in range(m):
+        while True:
