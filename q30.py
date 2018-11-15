a8bd975c87040b708679402399fa5083a80450d8
Ishan Khandelwal <khandelwalishan@rocketmail.com>
2018-11-15 13:14:22 +0530

    Update q30.py

--- q30.py
+++ q30.py
@@ -2,4 +2,4 @@ relatives={"Lisa":"daughter", "Bart":"son", "Marge":"mother", "Homer":"father", 
 ages={"Lisa":8,"Bart":10,"Marge":35,"Homer":40,"Santa":2}
 print("The Simpsons")
 for i in relatives:
-  print(i, "is a", relatives.get(i), "and is" ages.get(i), "years old")
+  print(i, "is a", relatives.get(i), "and is" ,ages.get(i), "years old")
